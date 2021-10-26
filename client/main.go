0463afdec5e77bf4f64c941292e08f5f192d669c
Christoffer Gram <chkg@itu.dk>
2021-10-26 12:06:29 +0200

    Add Publish Functionality to Client
    No lamport time stamps

--- client/main.go
+++ client/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/toffernator/chitty-chat/client/bindings"
@@ -12,6 +17,7 @@ import (
 
 const (
 	serverHost = "localhost:50051"
+	address    = "localhost:50052"
 )
 
 type Client struct {
@@ -27,19 +33,60 @@ func (c *Client) Broadcast(ctx context.Context, in *bindings.Message) (*bindings
 	}, nil
 }
 
+func publish(msg string, conn *grpc.ClientConn) {
+	client := bindings.NewClientToServerServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client.Publish(ctx, &bindings.Message{
+		LamportTs: 0,
+		Sender:    address,
+		Contents:  msg,
+	})
+}
+
+func serve() {
+	// Set-up client "service"
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	clientServer := Client{}
+
+	bindings.RegisterClientToServerServiceServer(s, clientServer)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
 func main() {
+	go serve()
+
+	// Connect to remote server
 	conn, err := grpc.Dial(serverHost, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	client := bindings.NewClientToServerServiceClient(conn)
-	time.Sleep(time.Duration(10) * time.Second)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	client.Publish(ctx, &bindings.Message{
-		LamportTs: 0,
-		Contents:  "hej",
-	})
+	// Parse user-input
+	for {
+		// Read user input
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			input := scanner.Text()
+
+			// Parse input
+			parsed := strings.SplitN(input, " ", 2)
+			fmt.Printf("%v\n", parsed)
+
+			// Act on user input
+			switch parsed[0] {
+			case "/publish":
+				publish(parsed[1], conn)
+			}
+		}
+	}
 }
